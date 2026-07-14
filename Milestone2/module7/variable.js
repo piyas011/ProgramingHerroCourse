bd3fe8926e80293e63b26fcf2c1cb016e86f86b8
Piyas Ahmed <apiyas761@gmail.com>
2026-07-14 22:05:15 +0600

    New js code added

---
+++ Milestone2/module7/variable.js
@@ -0,0 +1,21 @@
+// VARIABLE
+var weight = 50;
+var temperature = 30;
+var people = 32;
+console.log(temperature);
+
+// let , const
+let price = 100;
+price = 50;
+price = 400;
+
+const PI = 3.14;
+// const fullName = "Piyas Ahmed";
+// console.log(fullName);
+console.log(price);
+
+///////////////////////////////////
+let firstName = "Piyas";
+let lastName = " Ahmed";
+let fullName = firstName + lastName;
+console.log(fullName);
