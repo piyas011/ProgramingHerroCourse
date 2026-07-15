0a54419befc1ced6fb1a25cf9daa04d500b8759c
Piyas Ahmed <apiyas761@gmail.com>
2026-07-15 13:36:46 +0600

    new file added

---
+++ Milestone2/module7/add.js
@@ -0,0 +1,12 @@
+const orangePrice = 30;
+const mangoPrice = 43;
+const totalPrice = orangePrice + mangoPrice;
+console.log(orangePrice);
+console.log(mangoPrice);
+console.log(totalPrice);
+
+
+let firstName = "Piyas";
+let lastName = " Ahmed";
+let fullName = firstName + lastName;
+console.log("full Name = "+ fullName);
