4164fd06b15e23dd1d20de9a116c5cff2e2e8615
Piyas Ahmed <apiyas761@gmail.com>
2026-07-15 13:17:59 +0600

    new code added

--- Milestone2/module7/variable.js
+++ Milestone2/module7/variable.js
@@ -48,3 +48,24 @@ let brb = "jasmin";
 
 //  const let = 26;
 //  console.log(let);
+
+// const false = "False";
+
+// 3. has to be in one word. can not contain any spage or gap
+// const her phone number = 24423232323;
+const her_phone_number = 4343434343; // snake case
+const herPhoneNumber = 3434343434; //camel case.usually this one we will use.
+const HerPhoneNumber = 343434343434343; // pascal case
+
+// 4. name cannot start with a number .but number could be used anywhere other then the first leatter 
+// const 3person = 3
+const person3 = 3;
+const p3erson = 3;
+
+// 5 other than letter name can start with either _ , $ . cannot use any other special character . *&#@
+
+// 6 . case sensitive
+
+let address = "katabon";
+let Address = "bandroban";
+let AddRess = "shalbon";
