7694f8a6b5b2a31b73cf13c69c561444fe2aeee0
Piyas Ahmed <apiyas761@gmail.com>
2026-07-15 14:54:19 +0600

    code add

--- Milestone2/module7/add.js
+++ Milestone2/module7/add.js
@@ -1,22 +1,51 @@
-const orangePrice = 30;
-const mangoPrice = 43;
-const totalPrice = orangePrice + mangoPrice;
-console.log(orangePrice);
-console.log(mangoPrice);
-console.log(totalPrice);
+// const orangePrice = 30;
+// const mangoPrice = 43;
+// const totalPrice = orangePrice + mangoPrice;
+// console.log(orangePrice);
+// console.log(mangoPrice);
+// console.log(totalPrice);
 
+// let firstName = "Piyas";
+// let lastName = " Ahmed";
+// let fullName = firstName + lastName;
+// console.log("full Name = "+ fullName);
 
-let firstName = "Piyas";
-let lastName = " Ahmed";
-let fullName = firstName + lastName;
-console.log("full Name = "+ fullName);
+// ///////////////////////
+// let number = "34";
+// console.log(number);
+// console.log(typeof number);
+// let number2 = 34;
+// console.log(number2);
+// let number3 = parseInt(number);
+// console.log(number3);
 
+//
+//
+//
+//
+//
+const intro = "Hello JavaScript!";
+const firstName = "Piyas ";
+const lastName = "Ahmed";
+const fullName = firstName + lastName;
+const goal = " I am excited to become a web Debeloper.";
 
-///////////////////////
-let number = "34";
-console.log(number);
-console.log(typeof number);
-let number2 = 34;
-console.log(number2);
-let number3 = parseInt(number);
-console.log(number3);
+console.log(intro);
+console.log("first Name =" + firstName);
+console.log("Last Name " + lastName);
+console.log("Full Name = " + fullName);
+
+let num1 = 25;
+let num2 = 10;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+console.log(`Addition = ${addition}`);
+console.log(`Subtraction = ${subtraction}`);
+console.log(`Multiplication = ${multiplication}`) ;
+console.log(`Divisioon = ${division}`);
+console.log(`Modulus = ${modulus}`);
