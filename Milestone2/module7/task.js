aeeff5b2fe425790d153f1b6ff0b2cc97d75ecb9
Piyas Ahmed <apiyas761@gmail.com>
2026-07-16 09:44:59 +0600

    task 1,2,3 add

---
+++ Milestone2/module7/task.js
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////
+// Task 1 : Store Personal Information
+let firstName = "Piyas ";
+let lastName = "Ahmed";
+let fullName = firstName + lastName;
+let age = 20;
+let favoriteColor = "Black";
+let isStudent = false;
+
+console.log(`
+    Store Personal Information
+    Name : ${fullName}
+    Age  : ${age};
+    Favorit Color : ${favoriteColor};
+    Student : ${isStudent};
+    `)
+
+// Task 2 : Sum of Two Numbers 
+let num1 = 25;
+let num2 = 15;
+let result =  num1 + num2 ;
+console.log(`
+    Sum Of Two Number
+     Result =  ${result}`);
+
+
+// Task 3 Basic Calculator
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division =  num1 / num2;
+division = division.toFixed(2)
+let modulus = num1 % num2;
+
+console.log(`
+    Basic Calculator 
+    
+    ${num1} + ${num2} = ${addition}
+    ${num1} - ${num2} = ${subtraction}
+    ${num1} * ${num2} = ${multiplication}
+    ${num1} / ${num2} = ${division}
+    ${num1} % ${num2} = ${modulus}
+    
+    `)
