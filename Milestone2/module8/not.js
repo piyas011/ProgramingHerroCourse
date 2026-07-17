e3b18573859240527ee44807e4a05a9ec8db0e6b
Piyas Ahmed <apiyas761@gmail.com>
2026-07-17 15:54:21 +0600

    new js code added

---
+++ Milestone2/module8/not.js
@@ -0,0 +1,13 @@
+const isLogIn = false;
+
+// if(isLogIn){
+//     console.log("Log In sucessFull");
+// }else{
+//     console.log("Please login")
+// }
+
+if(!isLogIn){
+    console.log("Log In sucessFull");
+}else{
+    console.log("Please login")
+}
