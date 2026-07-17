75975a552640ffe285f257eb8cb040c36d590d05
Piyas Ahmed <apiyas761@gmail.com>
2026-07-17 12:09:50 +0600

    else condition

---
+++ Milestone2/module8/multipel.js
@@ -0,0 +1,16 @@
+const salary = 20026;
+const isBCS = true;
+const hasCar = false;
+
+
+// if(salary > 50000 && isBCS === true && hasCar  ){
+//     console.log("su Patro");
+// }else{
+//     console.log("goribs");
+// }
+
+if(salary >20000 || hasCar || isBCS ){
+    console.log("Chele valo");
+}else{
+    console.log("goribs");
+}
