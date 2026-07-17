3dd7493182cb41124a79f6de589264ff1506f4a7
Piyas Ahmed <apiyas761@gmail.com>
2026-07-17 15:09:03 +0600

    new code added

---
+++ Milestone2/module8/nasted.js
@@ -0,0 +1,38 @@
+const mony = 6000;
+
+// if(mony > 500){
+//     console.log("Boro loks");
+//  if(mony > 500){
+//     console.log(mony, "Taka koi paili tui");
+//  }else{
+//     console.log(mony ,"Tao to onek taka ache chole party dibi");
+//  }
+// }
+// else{
+//     console.log("Sob Plapain   Gorib's");
+// }
+
+const userName = "piyas";
+const password = 2278;
+// let isStudent = false;
+// let isJobHolder = true;
+// let salaryRang = 20000;
+
+if (userName === "piyas" && password === 2278) {
+  console.log("Log In sucessfull");
+} else {
+  console.log("Log In Faild Plese Try Again");
+}
+
+const age = 21;
+const hasVoterIdCard = false;
+
+if(age >= 18){
+    if(hasVoterIdCard){
+        console.log("you can vote Now");
+    }else{
+        console.log("you can not vote plese get your id card to vote");
+    }
+}else{
+    console.log("You can not vote right now ");
+}
