1b54c318340913b12ef479a62cf202d264ad311c
Piyas Ahmed <apiyas761@gmail.com>
2026-07-17 11:45:37 +0600

    js else condition

---
+++ Milestone2/module8/else.js
@@ -0,0 +1,30 @@
+/**
+ * if it is taining
+ * i will take the umbrella
+ * if not
+ * i will take extra books in my hand
+ * 
+ * if(condition){
+ *   
+ * }else{
+ * 
+ * }
+ * 
+ */
+const rickshawFare = 200;
+if(rickshawFare < 50){
+    console.log("i will go ");
+    console.log("go slowly");
+}else{
+    console.log("walking is also exercisiing");
+    console.log("I am a situational runner");
+}
+
+const x = 200;
+const y = 500;
+if(x !== y){
+    const z = x + y;
+    console.log("value of z : ",z);
+}else{
+    console.log("The are equal equal ");
+}
