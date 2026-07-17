32884036eb6e92acab70e08f559e79f63fc92d4e
Piyas Ahmed <apiyas761@gmail.com>
2026-07-17 11:21:56 +0600

    if  condition

---
+++ Milestone2/module8/if.js
@@ -0,0 +1,31 @@
+// condition
+if(true){
+    console.log("Inside tha condition");
+}
+
+if(false){
+    console.log("Hello javaScript");
+}
+if(6>4){
+    console.log("6 is bigger then 4");
+}
+if (50 <= 55){
+    console.log("coming inside the condition");
+}
+if (50 <= 50){
+    console.log(true);
+}
+
+let money = 10000;
+let price = 500;
+if(price < 1000){
+    console.log("Price Is lower");
+}
+let price2 = 1500;
+if(price < price2) {
+    console.log("Price 1 is lower ");
+}
+
+if(price < money){
+    console.log("i will buy it");
+};
