38b61729300f29b01681e566377d1947cf554829
Piyas Ahmed <apiyas761@gmail.com>
2026-07-17 15:29:43 +0600

    javascript tarnaty operator

---
+++ Milestone2/module8/tarnary.js
@@ -0,0 +1,26 @@
+/**
+ * Tarnary
+ * condition ? if true : if false;
+ */
+
+// const age = 21;
+// if(age >= 18){
+//     console.log("You can vote now");
+// }else{
+//     console.log("you can't vote !")
+// }
+
+// age >= 18 ? console.log("You can vote") : console.log("You can not vote");
+
+let price = 500;
+const isLeader = false;
+
+// if(isLeader){
+//     price = 0;
+// }else{
+//     price = price + 100;
+// }
+// console.log(price);
+
+// price = isLeader ? 0 : (price = price + 100);
+// console.log(price);
