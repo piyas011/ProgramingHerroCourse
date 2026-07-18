ad73be5c26437419b3c69b1f7749f11f6a1f4f98
Piyas Ahmed <apiyas761@gmail.com>
2026-07-18 22:45:18 +0600

    Task

---
+++ Milestone2/module9/task.js
@@ -0,0 +1,16 @@
+// Print 0 to 100;
+for(let i = 0; i <= 100; i++){
+    console.log(i);
+}
+
+
+// Print Every 3 Number 0 to 100
+for(let x = 1; x <= 100; x += 2){
+    console.log(x);
+}
+console.log("End");
+
+// print all the even number between 20 to 30
+for(let index = 20; index <= 30; index += 2){
+    console.log(index);
+}
