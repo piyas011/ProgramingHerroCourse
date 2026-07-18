6fcb0006e27afdefdd271b24d6f6e57f09dce1c3
Piyas Ahmed <apiyas761@gmail.com>
2026-07-18 22:10:29 +0600

    new code add

--- Milestone2/module9/even.js
+++ Milestone2/module9/even.js
@@ -10,3 +10,11 @@
 for(let x = 0; x <= 100; x += 2){
     console.log(x);
 }
+for(let x = 0; x <= 1000; x += 10){
+    console.log(x);
+}
+
+//Even Number 
+for(let i =2;i <= 1000; i += 2){
+    console.log(i);
+}
