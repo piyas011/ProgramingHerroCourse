f33ded85a171c27bf08ae8b7761832e02c819869
Piyas Ahmed <apiyas761@gmail.com>
2026-07-18 22:07:27 +0600

    learn for loop

---
+++ Milestone2/module9/even.js
@@ -0,0 +1,12 @@
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+
+// for(let x = -40; x<= 100; x++){
+//     console.log(x);
+// };
+
+for(let x = 0; x <= 100; x += 2){
+    console.log(x);
+}
---
+++ Milestone2/module9/while.js
@@ -0,0 +1,25 @@
+// while loop
+
+/**
+ * initialization;
+ * while(condition){
+ * //loop body
+ * increment/decrement;
+ * }
+ */
+for(let x = 1; x <= 100; x++){
+    console.log(x);
+}
+
+let count = 1;
+while(count <= 5){
+    console.log(count);
+    count++
+};
+
+let x = 50;
+while(x >= 1){
+    console.log(x);
+    x--
+
+}
