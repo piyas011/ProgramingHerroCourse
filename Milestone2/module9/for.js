8bebc7104d34f0ea98c6d9c071076929e811f2ae
Piyas Ahmed <apiyas761@gmail.com>
2026-07-18 21:30:14 +0600

    use for loop , Print Number 100 to 1

---
+++ Milestone2/module9/for.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * for(initialization; condition; modification){
+ * repetitive
+ * } 
+ *
+ *
+ */
+
+// for(let  i = 1; i <=100 ; i++){
+//     console.log("Value of i = ",i);
+// };
+
+for(let  i = 100; i >= 1 ; i--){
+    console.log("Value of i = ",i);
+};
