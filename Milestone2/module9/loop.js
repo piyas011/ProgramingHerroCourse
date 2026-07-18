0db81690541913cdb4900e61d629a64d15e5af5c
Piyas Ahmed <apiyas761@gmail.com>
2026-07-18 21:21:12 +0600

    javascript loop

---
+++ Milestone2/module9/loop.js
@@ -0,0 +1,13 @@
+// const count = 0;
+// let count = 0;
+// conunt < 10;
+// count ++
+// console.log("do something");
+
+// for(let count = 1; count <= 10 ; count++){
+//     console.log("Hello World", count);
+// }
+
+for(let x = 1; x <= 100; x++){
+    console.log("Hello World", x);
+};
