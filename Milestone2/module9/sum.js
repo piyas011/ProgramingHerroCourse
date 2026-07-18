6b5d29451cbffd6a0138f090a2a466cafb7ef898
Piyas Ahmed <apiyas761@gmail.com>
2026-07-18 22:32:21 +0600

    new code add

--- Milestone2/module9/sum.js
+++ Milestone2/module9/sum.js
@@ -1,8 +1,29 @@
-let result = 0;
+// let result = 0;
 
-for(let i = 2; i <= 10; i += 2){
+// for(let i = 2; i <= 10; i += 2){
+//     console.log(i);
+//     result = result + i;
+// };
+
+// console.log("Result = " ,result);
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+for(let i = 10; i >= 1; i -= 2){
     console.log(i);
-    result = result + i;
+}
+console.log("end");
+
+
+//////////////////////////////////
+
+const number =  3;
+for(let i = 1; i <= 10; i++){
+    console.log(`${number} * ${i} = ${number*i}`);
 };
 
-console.log("Result = " ,result);
+const number2 = 100;
+for(let x = 1; x <= 10 ; x++){
+    console.log(`${number2} * ${x} = ${number2*x}`);
+};
