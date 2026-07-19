4bd4a53be3692404bf9a3d96aa77e6bc5d78a942
Piyas Ahmed <apiyas761@gmail.com>
2026-07-19 15:37:49 +0600

    shift and unshift

---
+++ Milestone2/module10/shift-unshift.js
@@ -0,0 +1,12 @@
+let number = [12,23,34,45,56,67,78,89,90];
+console.log(number);
+
+number.shift();
+console.log(number);
+
+number.shift();
+console.log(number);
+
+
+number.unshift(2323);
+console.log(number);
