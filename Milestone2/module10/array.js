c5efb17706f05b5445d94c3de6641be3602426a0
Piyas Ahmed <apiyas761@gmail.com>
2026-07-19 15:03:40 +0600

    javaScript Array

--- Milestone2/module10/array.js
+++ Milestone2/module10/array.js
@@ -5,10 +5,21 @@
  * 4.
  */
 
-const price = 34;
-const studentName = "Piyas Ahmed";
-const isRich = true 
+// const price = 34;
+// const studentName = "Piyas Ahmed";
+// const isRich = true 
 
-for(let i = 1; i< 10; i++){
-    console.log(i);
-}
+// for(let i = 1; i< 10; i++){
+//     console.log(i);
+// }
+
+//variable can hold only one value . one data
+const player1 = 23;
+const player2 = 34;
+const player3 = 45;
+const player4 = 54;
+
+
+// array can hold more than one value
+const team = [123, 34 ,45,56,67,78,765,54,34];
+const players = ["jamit","korin","rohim","lalmia"];
