7296808c27aff4aae21dea0078e63f4212cf1265
Piyas Ahmed <apiyas761@gmail.com>
2026-07-19 15:18:28 +0600

    array index

---
+++ Milestone2/module10/index.js
@@ -0,0 +1,13 @@
+// Index
+const players = ["Rohim","Korim","jamil","babul"];
+console.log(players.length);
+const player1 = players[0];
+const player4 = players[3];
+console.log(player4)
+console.log(player1);
+console.log(players.length);
+console.log(players[players.length -1]);
+
+players[players.length] = "Piyas Ahmed";
+console.log(players);
+console.log(players.length);
---
+++ Milestone2/module10/length.js
@@ -0,0 +1,4 @@
+const fruits = ["mango ","lychee","jackFruit","papaya","grape","grapeFruit"];
+
+const totalFruits = fruits.length;
+console.log(totalFruits);
