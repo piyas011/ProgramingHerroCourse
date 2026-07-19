efd368d30e227aacc45723763ec811d4d96b7002
Piyas Ahmed <apiyas761@gmail.com>
2026-07-19 20:00:57 +0600

    new code add

---
+++ Milestone2/module10/loopArray.js
@@ -0,0 +1,10 @@
+const numbers = [34,45,56,767,64,43,23,342,12,434,45,];
+
+
+let sum = 0;
+for(const number of numbers){
+    // console.log(`Currnet item ${number}`);
+   sum = sum + number;
+}
+console.log(sum);
+
---
+++ Milestone2/module10/while.js
@@ -0,0 +1,13 @@
+const numbers =[23,23,34,45,76,534,34,32,32];
+
+let i =0;
+
+while(i < numbers.length){
+    console.log(numbers[i]);
+    i++;
+};
+
+const furits = ["apple","orenge"];
+for(const item of furits){
+    console.log(item);
+}
