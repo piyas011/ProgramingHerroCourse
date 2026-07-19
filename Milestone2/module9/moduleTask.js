5f1851e3cdea91860bb23e59338143eff96992ac
Piyas Ahmed <apiyas761@gmail.com>
2026-07-19 09:52:17 +0600

    task

---
+++ Milestone2/module9/moduleTask.js
@@ -0,0 +1,52 @@
+// print "Hello" Five times
+// write a for loop that prints "hello" five times
+// for(let i = 1; i <= 10;i++){
+//     console.log(i,"Hello");
+// };
+
+// Task 2
+// for (let i = 1; i < 10; i++) {
+//   console.log(i);
+// }
+
+
+// Task 3
+
+// let countdown = 10
+// while(countdown >= 0){
+//     console.log(countdown);
+//     countdown --
+// }
+
+// task 4
+// for(let x = 1; x<= 100;x++){
+//     if(x %2 === 0){
+//         console.log(x)
+//     }
+
+// }
+
+
+// Task 4 sum of Number
+// let sum = 0;
+// for(let num = 1;num <= 20; num++){
+//     sum = sum + num;
+// }
+// console.log(sum);
+
+    
+// // Task 5
+// let multiplication = 8;
+// for(let i = 1;i <= 10; i++){
+//     console.log(`${multiplication} * ${i} = ${multiplication*i}`);
+// }
+
+
+// task 6 sum of even number;
+let sum =  0;
+for(let i = 0;i <=50; i++){
+    if(i %2 === 0){
+        sum = sum + i;
+    }
+}
+console.log("Sum = ",sum);
