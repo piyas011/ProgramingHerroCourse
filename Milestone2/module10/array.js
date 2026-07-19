5051fac86e6fde58ada656875ab328bed546eb5d
Piyas Ahmed <apiyas761@gmail.com>
2026-07-19 15:00:07 +0600

    javaScript Array

---
+++ Milestone2/module10/array.js
@@ -0,0 +1,14 @@
+/**
+ * 1. Variable
+ * 2. conditionals
+ * 3. loop:for,while,do while
+ * 4.
+ */
+
+const price = 34;
+const studentName = "Piyas Ahmed";
+const isRich = true 
+
+for(let i = 1; i< 10; i++){
+    console.log(i);
+}
