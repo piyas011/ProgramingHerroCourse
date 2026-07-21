efbb75a666f55682b140d13b6e7093e2f0097363
Piyas Ahmed <apiyas761@gmail.com>
2026-07-21 20:33:28 +0600

    module 11-1

---
+++ Milestone2/module11/string.js
@@ -0,0 +1,11 @@
+const cup = "World cup";
+const tea = "Tea cup";
+const location = `Dhaka at mitpur`;
+
+const number = [23, 34, 5, 67, 78, 89];
+console.log(number.length);
+
+console.log(location.length);
+console.log(cup.length);
+console.log(number[0]);
+console.log(location[0]);
