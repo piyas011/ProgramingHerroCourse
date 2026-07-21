4c05547524faea52f6df8f10e19df4a09a32c3e2
Piyas Ahmed <apiyas761@gmail.com>
2026-07-21 20:20:51 +0600

    somting code added

--- Milestone2/module10/module10Task.js
+++ Milestone2/module10/module10Task.js
@@ -1,19 +1,17 @@
 // Task 1
-const friends = ["Piyas", "Rohim","Korim","Solim"];
+const friends = ["Piyas", "Rohim", "Korim", "Solim"];
 console.log(friends);
 
 // Task 2
 const friendsLength = friends.length;
 console.log("Total Friends =", friendsLength);
 
-
 // Task 3
 const index = friends[2];
 console.log(index);
 
-
 // Task 4
-const chang = friends[0] = "Rahul";
+const chang = (friends[0] = "Rahul");
 console.log(friends);
 
 // Task 5
@@ -28,7 +26,7 @@ console.log(friends);
 friends.unshift("Piyas Ahmed");
 console.log(friends);
 
-// Task 8 
+// Task 8
 friends.shift();
 console.log(friends);
 
@@ -37,33 +35,28 @@ let includ = friends.includes("Rohim");
 console.log(includ);
 
 // Task 10
-  let indexof = friends.indexOf("Rohim");
-  console.log(indexof);
-    
-// Task 11 
-for(let friend of friends){
-    console.log(friend);
+let indexof = friends.indexOf("Rohim");
+console.log(indexof);
+
+// Task 11
+for (let friend of friends) {
+  console.log(friend);
 }
 console.log("End");
-for(let i = 0 ; i <= friends.length-1; i++){
-    console.log(friends[i]);
+for (let i = 0; i <= friends.length - 1; i++) {
+  console.log(friends[i]);
 }
 console.log("End");
-let i = 0
-while(i <= friends.length-1){
-    console.log(friends[i]);
-    i++;
+let i = 0;
+while (i <= friends.length - 1) {
+  console.log(friends[i]);
+  i++;
 }
-console.log("__________________")
+console.log("__________________");
 let x = 0;
-do{
-console.log(friends[x])
-x++
-}while(
-x <= friends.length -1
-);
-
-
-
+do {
+  console.log(friends[x]);
+  x++;
+} while (x <= friends.length - 1);
 
 // Task 12
