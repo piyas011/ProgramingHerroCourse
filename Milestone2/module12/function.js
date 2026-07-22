062d9f460e2d4f85d132754aa9808784843b940f
Piyas Ahmed <apiyas761@gmail.com>
2026-07-22 20:55:13 +0600

    js function

---
+++ Milestone2/module12/function.js
@@ -0,0 +1,17 @@
+/**
+ *function
+ give a name 
+give two bracket
+after first bracket two second bracket
+inside the second bracket you will do the work for this function.
+ */
+function firstFunction() {
+  console.log("Hello World");
+  console.log("Hello this is a function");
+}
+firstFunction();
+
+function secoundFunction() {
+  console.log("hello this is secound function");
+}
+secoundFunction();
---
+++ Milestone2/module12/revition.js
@@ -0,0 +1,15 @@
+/**
+ * Variable
+ * conditionals
+ * loop
+ * array
+ * objects
+ *
+ */
+const price = 10;
+if (price > 100) {
+  console.log("I will buy it");
+}
+for (let x = 0; x <= 100; x++) {
+  console.log(x);
+}
