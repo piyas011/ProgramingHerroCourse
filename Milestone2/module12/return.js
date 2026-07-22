4136f4e473c0373286f59f964f08fcb0c6490682
Piyas Ahmed <apiyas761@gmail.com>
2026-07-22 21:56:52 +0600

    function return

---
+++ Milestone2/module12/return.js
@@ -0,0 +1,17 @@
+function cutHalf(number) {
+  const half = number / 2;
+  return half;
+}
+
+const result = cutHalf(100);
+console.log(result);
+
+console.log("==========================");
+
+function sum(a, b) {
+  const sum = a + b;
+  return sum;
+}
+
+const result2 = sum(23, 23);
+console.log(result2);
