d6a88cc8b8fc3ab3ebf4ddc576648dd0d49e32d4
Piyas Ahmed <apiyas761@gmail.com>
2026-07-22 10:54:06 +0600

    object git push

--- Milestone2/module11/object.js
+++ Milestone2/module11/object.js
@@ -4,3 +4,21 @@ const student = {
   roll: "Ph-2321",
   acrive: true,
 };
+
+const car = {
+  nodel: "BMW",
+  color: "Black",
+  price: 2500000,
+};
+console.log(car);
+console.log(car.nodel);
+
+const person = {
+  name: "Rohim Mia",
+  profession: "student",
+  age: 21,
+  isSigle: true,
+};
+
+console.log(person["name"]);
+console.log(person.age);
