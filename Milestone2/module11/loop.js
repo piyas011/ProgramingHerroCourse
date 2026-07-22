5fd98f625c977101b4ce60e59d8eb6dbdc58f29f
Piyas Ahmed <apiyas761@gmail.com>
2026-07-22 12:44:01 +0600

    new code add

---
+++ Milestone2/module11/loop.js
@@ -0,0 +1,16 @@
+const car = {
+  nodel: "BMW",
+  color: "Black",
+  price: 2500000,
+};
+const person = {
+  name: "Rohim Mia",
+  profession: "student",
+  age: 21,
+  isSigle: true,
+};
+
+// for (const key in person) {
+//   console.log(person[key]);
+// }
+console.log(person["age"]);
