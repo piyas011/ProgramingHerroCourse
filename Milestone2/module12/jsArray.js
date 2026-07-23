1e2a3edcbac3bd5af9e1f31d9ced4a3741c43b5f
Piyas Ahmed <apiyas761@gmail.com>
2026-07-23 19:17:39 +0600

    js function

--- Milestone2/module12/jsArray.js
+++ Milestone2/module12/jsArray.js
@@ -1,7 +1,20 @@
-const numbers = [23, 45, 576, 78, 89, 34, 23, 34];
-let sum = 0;
-for (const num of numbers) {
-  //   console.log(num);
-  sum = sum + num;
-}
-console.log("Total Sum of Number :", sum);
+// const numbers = [23, 45, 576, 78, 89, 34, 23, 34];
+// let sum = 0;
+// for (const num of numbers) {
+//   //   console.log(num);
+//   sum = sum + num;
+// }
+// console.log("Total Sum of Number :", sum);
+// console.log("============================");
+
+// const numbers = [12, 34, 45, 56, 67, 78];
+// function sumOfArray(numbers) {
+//   let sum = 0;
+//   for (const x of numbers) {
+//     sum = sum + x;
+//   }
+//   return sum;
+// }
+
+// let result = sumOfArray(numbers);
+// console.log(`Total Sum of array :${result}`);
