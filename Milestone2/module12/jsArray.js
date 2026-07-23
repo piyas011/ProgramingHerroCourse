9741d5fbf709fcf692a044a69b2605a47ed647a0
Piyas Ahmed <apiyas761@gmail.com>
2026-07-23 19:10:42 +0600

    new code

---
+++ Milestone2/module12/jsArray.js
@@ -0,0 +1,7 @@
+const numbers = [23, 45, 576, 78, 89, 34, 23, 34];
+let sum = 0;
+for (const num of numbers) {
+  //   console.log(num);
+  sum = sum + num;
+}
+console.log("Total Sum of Number :", sum);
