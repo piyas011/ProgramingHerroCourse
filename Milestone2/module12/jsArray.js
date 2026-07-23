bd1f387738a8469dd315be8c3d67de3b46b039b2
Piyas Ahmed <apiyas761@gmail.com>
2026-07-23 20:08:40 +0600

    new code

--- Milestone2/module12/jsArray.js
+++ Milestone2/module12/jsArray.js
@@ -18,3 +18,44 @@
 
 // let result = sumOfArray(numbers);
 // console.log(`Total Sum of array :${result}`);
+
+// const newArray = [2, 2, 2, 2, 2];
+
+// function sumOfArray(array) {
+//   let sum = 0;
+//   for (const num of array) {
+//     sum = sum + num;
+//   }
+//   return sum;
+// }
+
+// const result = sumOfArray(newArray);
+// console.log(result);
+
+// console.log("==========================");
+
+// const array = [34, 45, 66, 78, 23, 45, 77, 43];
+// let evenArray = [];
+// let oddArray = [];
+// let evenNumberSum = 0;
+// let oddNumberSum = 0;
+
+// function evenOdd(array) {
+//   console.log(array);
+//   for (const num of array) {
+//     if (num % 2 === 0) {
+//       evenArray.push(num);
+//       evenNumberSum = evenNumberSum + num;
+//     } else {
+//       oddArray.push(num);
+//       oddNumberSum = oddNumberSum + num;
+//     }
+//   }
+// }
+
+// evenOdd(array);
+// console.log(`Main Array : ${array}`);
+// console.log(`Even Number Array : ${evenArray}`);
+// console.log(`Odd Number Array : ${oddArray}`);
+// console.log(`${evenArray} = ${evenNumberSum}`);
+// console.log(`${oddArray} = ${oddNumberSum}`);
