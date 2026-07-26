9f5e5860a1dbdce06e953c127dd747c4030d484f
Piyas Ahmed <apiyas761@gmail.com>
2026-07-26 21:18:07 +0600

    FizzBuzz Check

--- Milestone2/module13/problem1.js
+++ Milestone2/module13/problem1.js
@@ -37,3 +37,28 @@
   const factorialNumber = getFactorialNumber(number);
   console.log(` The Factorial Number Of ${number} = ${factorialNumber}`);
 }
+
+{
+  /**
+   * Problem 4 | FizzBuzz (1 to n)
+   * Loop through Number from 1 to n . If a number is divisible by 3, Print "Fizz", if divisible by 5, Print "Buzz"; if divisible by both. Print "FizzBuzz"; otherwise print the number itself.
+   */
+
+  let nNumber = 23; //use prompt();
+  function divisibleCheck(n) {
+    let result;
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        result = "FizzBuzz";
+      } else if (i % 3 === 0) {
+        result = "Fizz";
+      } else if (i % 5 === 0) {
+        result = "Buzz";
+      } else {
+        result = i;
+      }
+      console.log(result);
+    }
+  }
+  divisibleChack(nNumber);
+}
