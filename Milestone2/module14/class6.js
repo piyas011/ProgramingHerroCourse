7b3db013e65fd03e66c608494fc784571357533d
Piyas Ahmed <apiyas761@gmail.com>
2026-07-27 21:42:27 +0600

    sum even number with array

---
+++ Milestone2/module14/class6.js
@@ -0,0 +1,38 @@
+{
+  /**
+   * using the vs code debugger
+   */
+  function findAverage(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    let average = total / numbers.length;
+    return average;
+  }
+  //   console.log(findAverage([10, 20, 30]));
+}
+
+{
+  //   function findAverage(numbers) {
+  //     let total = 0;
+  //     for (const num of numbers) {
+  //       total += num;
+  //     }
+  //     let average = total / numbers.length;
+  //     return average;
+  //   }
+  //   console.log(findAverage([10, 20, 30]));
+}
+{
+  function sumEvenNumber(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        total += numbers[i];
+      }
+    }
+    return total;
+  }
+  console.log(sumEvenNumber([20, 61, 15, 80]));
+}
