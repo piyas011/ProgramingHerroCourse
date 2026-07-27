4ecee795189d0c2b8e7ec05c8283058b9f84a027
Piyas Ahmed <apiyas761@gmail.com>
2026-07-27 20:38:46 +0600

    Introduce Yourself

--- Milestone2/module14/class5.js
+++ Milestone2/module14/class5.js
@@ -12,3 +12,19 @@
   console.log(`${finalAmount + 20}`);
 }
 console.log("===================================");
+{
+  /**
+   * Problem 2 : introduce(name,birthYear,city)
+   * Purpose : should calculate age from brithYear (current Yeay - birthYear)
+   * and return an sentence like
+   * "my name is alex, I am 20 Years old from dhaka,"
+   */
+
+  let currentYear = 2026;
+  function introduce(name, birthYear, city) {
+    let age = currentYear - birthYear;
+    return `Hello My Name Is ${name} and I am ${age} Years old and I am From ${city}`;
+  }
+  let result = introduce("Piyas Ahmed", 2005, "Dhaka");
+  console.log(result);
+}
