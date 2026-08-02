5daf9d228235764407cfd94c78fcc41a3d6eb8db
Piyas Ahmed <apiyas761@gmail.com>
2026-08-02 17:07:58 +0600

    Class 3

---
+++ Milestone2/module16/class3.js
@@ -0,0 +1,10 @@
+let name = `Piyas Ahmed`;
+let location = `Mirpur Tamanna Park , Istran Howsing`;
+let bill = 500;
+let quantity = 5;
+let message = `hello Mr.${name} , Your bill is ${bill * quantity} and location ${location} 
+line 1
+line 2
+line 3 
+line 4`;
+console.log(message);
