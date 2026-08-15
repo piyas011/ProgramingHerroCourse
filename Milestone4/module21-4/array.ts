1b9f56b7f66501f045b03658728b30db497f6ca2
Piyas Ahmed <apiyas761@gmail.com>
2026-08-15 11:06:53 +0600

    Somthing

---
+++ Milestone4/module21-4/array.ts
@@ -0,0 +1,16 @@
+const number: number[] = [12, 23, 34, 434, 56];
+const books: string[] = ["", ""];
+
+const book: {
+  name: string;
+  author: string;
+  price: number;
+  pages: number;
+} = {
+  name: "chemistry",
+  author: "Naag",
+  price: 500,
+  pages: 200,
+};
+
+console.log(book.name);
---
+++ Milestone4/module21-4/object.ts
@@ -0,0 +1,5 @@
+const student: { name: string; age: number; isPassed: boolean } = {
+  name: "Piyas ",
+  age: 21,
+  isPassed: true,
+};
