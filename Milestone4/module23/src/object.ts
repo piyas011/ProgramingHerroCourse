22d43b4a38adac5b8f47a618037ebe2afff9f3ea
Piyas Ahmed <apiyas761@gmail.com>
2026-08-18 18:09:25 +0600

    somthing

--- Milestone4/module23/src/object.ts
+++ Milestone4/module23/src/object.ts
@@ -4,11 +4,26 @@ const piyasAhmed = {
   email: "apiyas221@gmail.com",
 };
 
-const createStudent = (name: string, age: number, email: string) => {
-  const obj = { name, age, email };
+const createStudent = (
+  name: string,
+  age: number,
+  email: string,
+  marks: number,
+) => {
+  const obj = {
+    name,
+    age,
+    email,
+    marks,
+    forgarePass() {
+      // somthing
+    },
+  };
   return obj;
 };
 
-const piyas = createStudent("Piyas", 21, "apiyas221@gmail.com");
+const piyas = createStudent("Piyas", 21, "apiyas221@gmail.com", 100);
+piyas.marks = 0;
+const rohim = createStudent("Rohim", 20, "rohim@gmail.com", 200);
 
-console.log(piyas);
+console.log(piyas, rohim);
