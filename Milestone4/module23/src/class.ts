45035e040b1096afde4a9f70c39a2f8716b933ab
Piyas Ahmed <apiyas761@gmail.com>
2026-08-19 10:37:27 +0600

    module23 oop class

--- Milestone4/module23/src/class.ts
+++ Milestone4/module23/src/class.ts
@@ -20,21 +20,49 @@
 // const she = new Student();
 // console.log(piyas, she);
 
+// class Student {
+//   name: string;
+//   age: number;
+//   email: string;
+//   marks: number;
+
+//   // constructor --> special method
+//   constructor(name: string, age: number, email: string, mark: number) {
+//     this.name = name;
+//     this.age = age;
+//     this.email = email;
+//     this.marks = mark;
+//   }
+// }
+
+// const piyas = new Student("Piyas", 21, "apiyas221@gmail.com", 100);
+// const she = new Student("Prity", 20, "prity@gmail.com", 200);
+// const mushu = new Student("Mishu", 18, "mishu@gmail.com", 100);
+// console.log(piyas, she, mushu);
+
 class Student {
   name: string;
   age: number;
-  email: string;
-  marks: number;
+  mark: number;
+  cgpa: number;
+  isActive: boolean;
 
-  // constructor --> special method
-  constructor(name: string, age: number, email: string, mark: number) {
+  constructor(
+    name: string,
+    age: number,
+    mark: number,
+    cgpa: number,
+    isActive: boolean,
+  ) {
     this.name = name;
     this.age = age;
-    this.email = email;
-    this.marks = mark;
+    this.mark = mark;
+    this.cgpa = cgpa;
+    this.isActive = isActive;
   }
 }
 
-const piyas = new Student("Piyas", 21, "apiyas221@gmail.com", 100);
-const she = new Student("Prity", 20, "prity@gmail.com", 200);
-console.log(piyas, she);
+const student1 = new Student("Piyas", 21, 100, 2.9, false);
+const student2 = new Student("Prity Akter", 20, 99, 2.8, true);
+
+console.log(student1, student2);
