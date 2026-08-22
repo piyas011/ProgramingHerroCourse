ffb4c23b4319f57b43d9a57b58742ddccd4db83c
Piyas Ahmed <apiyas761@gmail.com>
2026-08-22 17:46:12 +0600

    OOP Class

---
+++ practice/src/class.ts
@@ -0,0 +1,19 @@
+/*
+  name
+  age
+  email
+  marks 
+ */
+
+class Student {
+  name: string = "Piyas Ahmed";
+  age: number = 21;
+  email: string = "apiyas221@gmail.com";
+  mark: number = 100;
+}
+
+// instantiate
+// instance
+const piyas = new Student();
+
+console.log(piyas);
