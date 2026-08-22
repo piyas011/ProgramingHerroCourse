40b676188e883b03b39d763f446b2fca8b40a7f2
Piyas Ahmed <apiyas761@gmail.com>
2026-08-22 18:05:31 +0600

    practice

--- practice/dist/class.js
+++ practice/dist/class.js
@@ -22,6 +22,41 @@ class Student {
 // instance
 const piyas = new Student("Piyas Ahmed", 21, "Piyas@piyas.com", 100);
 const prity = new Student("Prity", 20, "prity@prity.com", 200);
-console.log(piyas);
-console.log(prity);
+// console.log(piyas);
+// console.log(prity);
+/////////////////////////////////////////////////////
+class User {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const user1 = new User("Piyas Ahmed", 21);
+const user2 = new User("Prity", 20);
+// console.log(user1, user2);
+class Car {
+    brand;
+    model;
+    year;
+    constructor(brand, model, year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+}
+const car1 = new Car("Toyora", "Corolla", 2024);
+// console.log(car1);
+class Product {
+    name;
+    price;
+    category;
+    constructor(name, price, category) {
+        ((this.name = name), (this.price = price));
+        this.category = category;
+    }
+}
+const mango = new Product("mango", 100, "fruits");
+console.log(mango);
 //# sourceMappingURL=class.js.map
--- practice/src/class.ts
+++ practice/src/class.ts
@@ -23,5 +23,51 @@ class Student {
 // instance
 const piyas = new Student("Piyas Ahmed", 21, "Piyas@piyas.com", 100);
 const prity = new Student("Prity", 20, "prity@prity.com", 200);
-console.log(piyas);
-console.log(prity);
+// console.log(piyas);
+// console.log(prity);
+
+/////////////////////////////////////////////////////
+
+class User {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const user1 = new User("Piyas Ahmed", 21);
+const user2 = new User("Prity", 20);
+
+// console.log(user1, user2);
+
+class Car {
+  brand: string;
+  model: string;
+  year: number;
+
+  constructor(brand: string, model: string, year: number) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+}
+
+const car1 = new Car("Toyora", "Corolla", 2024);
+// console.log(car1);
+
+class Product {
+  name: string;
+  price: number;
+  category: string;
+
+  constructor(name: string, price: number, category: string) {
+    ((this.name = name), (this.price = price));
+    this.category = category;
+  }
+}
+
+const mango = new Product("mango", 100, "fruits");
+console.log(mango);
