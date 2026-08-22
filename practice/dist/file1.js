26810050926a285b66e49af7e569cd67a15fdd47
Piyas Ahmed <apiyas761@gmail.com>
2026-08-22 17:26:48 +0600

    practice

--- practice/dist/file1.js
+++ practice/dist/file1.js
@@ -2,4 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const hello = "hello this is my typeScript practice file";
 console.log(hello);
+const student1name = "Piyas Ahmed";
+const student1age = 21;
+const student1email = "apiyas221@gmail.com";
+const student2name = "Prity";
+const student2age = 20;
+const student2email = "prity@prity.com";
+/////////////////////////////////////////////////
+const piyas = {
+    name: "Piyas Ahmed",
+    age: 21,
+    email: "apiyas221@gmail.com",
+    phoneNumber: 123434534,
+};
+console.log(piyas);
+console.log(piyas.name);
+console.log(piyas.age);
+console.log(piyas.email);
+console.log(piyas.phoneNumber);
+const prity = {
+    name: "Prity Akter",
+    age: 20,
+    email: "prity@prity.com",
+    phoneNumber: 43434343,
+};
+console.log(prity);
+console.log(prity.name);
+console.log(prity.age);
+console.log(prity.email);
+console.log(prity.phoneNumber);
 //# sourceMappingURL=file1.js.map
--- practice/src/file1.ts
+++ practice/src/file1.ts
@@ -1,2 +1,38 @@
 const hello: string = "hello this is my typeScript practice file";
 console.log(hello);
+
+const student1name: string = "Piyas Ahmed";
+const student1age: number = 21;
+const student1email: string = "apiyas221@gmail.com";
+
+const student2name: string = "Prity";
+const student2age: number = 20;
+const student2email: string = "prity@prity.com";
+
+/////////////////////////////////////////////////
+
+const piyas = {
+  name: "Piyas Ahmed",
+  age: 21,
+  email: "apiyas221@gmail.com",
+  phoneNumber: 123434534,
+};
+
+console.log(piyas);
+console.log(piyas.name);
+console.log(piyas.age);
+console.log(piyas.email);
+console.log(piyas.phoneNumber);
+
+const prity = {
+  name: "Prity Akter",
+  age: 20,
+  email: "prity@prity.com",
+  phoneNumber: 43434343,
+};
+
+console.log(prity);
+console.log(prity.name);
+console.log(prity.age);
+console.log(prity.email);
+console.log(prity.phoneNumber);
