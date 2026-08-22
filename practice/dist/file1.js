283eba572805c0de01299b1c5a69ebc27946a3a7
Piyas Ahmed <apiyas761@gmail.com>
2026-08-22 16:56:36 +0600

    pracrice file

---
+++ practice/dist/file1.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello = "hello this is my typeScript practice file";
+console.log(hello);
+//# sourceMappingURL=file1.js.map
---
+++ practice/src/file1.ts
@@ -0,0 +1,2 @@
+const hello: string = "hello this is my typeScript practice file";
+console.log(hello);
