c3250ca55d0499f419215f33720808b3c1cf3bc6
Piyas Ahmed <apiyas761@gmail.com>
2026-09-02 16:07:56 +0600

    Practice

---
+++ Milestone5/module27/my-react-ts-app/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "./todo";
+import Todo from "./todo";
+
+function App() {
+  return (
+    <>
+      <h1>Get started With Me</h1>
+      <h2>Hello</h2>
+      <Todo name="Piyas" age="21"></Todo>
+      <Todo name="Prity" age="20"></Todo>
+    </>
+  );
+}
+
+export default App;
