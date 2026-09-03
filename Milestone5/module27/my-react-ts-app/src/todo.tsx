f7c05b607ab87e3397769eec33b850e153214a15
Piyas Ahmed <apiyas761@gmail.com>
2026-09-03 14:46:04 +0600

    somthing

--- Milestone5/module27/my-react-ts-app/src/todo.tsx
+++ Milestone5/module27/my-react-ts-app/src/todo.tsx
@@ -1,17 +1,17 @@
-interface TodoType {
-  name: string;
-  age: string;
-}
+// interface TodoType {
+//   name: string;
+//   age: string;
+// }
 
-function Todo({ name, age }: TodoType) {
-  return (
-    <div>
-      <h1>Hello this is my First React app</h1>
-      {/* <h2>My Name Is : {props.name}</h2> */}
-      <h2>My Name Is : {name}</h2>
-      <h2>Age : {age}</h2>
-    </div>
-  );
-}
+// function Todo({ name, age }: TodoType) {
+//   return (
+//     <div>
+//       <h1>Hello this is my First React app</h1>
+//       {/* <h2>My Name Is : {props.name}</h2> */}
+//       <h2>My Name Is : {name}</h2>
+//       <h2>Age : {age}</h2>
+//     </div>
+//   );
+// }
 
-export default Todo;
+// export default Todo;
