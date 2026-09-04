4982a7a7cad8d736e7843c0a47c75fc8dd393cbb
Piyas Ahmed <apiyas761@gmail.com>
2026-09-04 11:31:57 +0600

    practice

--- Milestone5/module29/my-first-project/src/App.tsx
+++ Milestone5/module29/my-first-project/src/App.tsx
@@ -3,6 +3,10 @@
 // import type { CountryType } from "./type";
 // import Countries from "./components/countries";
 
+import { Suspense } from "react";
+import type { CountryType } from "./type";
+import Countries from "./components/countries";
+
 // // Create a Promise to load data
 // const countriesPromise = async (): Promise<CountryType[]> => {
 //   const response = await fetch("https://openapi.programming-hero.com/api/all");
@@ -25,3 +29,26 @@
 // }
 
 // export default App;
+
+// Create Promise to Lode Data
+
+const countriesPromise = async (): Promise<CountryType[]> => {
+  const response = await fetch("https://openapi.programming-hero.com/api/all");
+  const data = await response.json();
+  return data.countries;
+};
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      {
+        <Suspense fallback={<h1>Data Loading...</h1>}>
+          <Countries countriesPromise={countriesPromise()}></Countries>
+        </Suspense>
+      }
+    </>
+  );
+}
+
+export default App;
--- Milestone5/module29/my-first-project/src/components/countries.tsx
+++ Milestone5/module29/my-first-project/src/components/countries.tsx
@@ -1,17 +1,34 @@
+// import { use } from "react";
+// import type { CountryType } from "../type";
+
 import { use } from "react";
 import type { CountryType } from "../type";
 
-export interface CountriesProps {
+// export interface CountriesProps {
+//   countriesPromise: Promise<CountryType[]>;
+// }
+
+// export default function Countries({ countriesPromise }: CountriesProps) {
+//   const countries = use(countriesPromise);
+//   console.log(countries);
+
+//   return (
+//     <div>
+//       <h2>Countries : {countries.length} </h2>
+//     </div>
+//   );
+// }
+
+export interface CountryProp {
   countriesPromise: Promise<CountryType[]>;
 }
 
-export default function Countries({ countriesPromise }: CountriesProps) {
+export default function Countries({ countriesPromise }: CountryProp) {
   const countries = use(countriesPromise);
   console.log(countries);
-
   return (
     <div>
-      <h2>Countries : {countries.length} </h2>
+      <h1>Total Country : {countries.length} </h1>
     </div>
   );
 }
