f35bc809f535aad600cbcbff803831f1ed6c607e
Piyas Ahmed <apiyas761@gmail.com>
2026-09-04 16:40:39 +0600

    practice

--- Milestone5/module29/my-first-project/src/components/country/country.tsx
+++ Milestone5/module29/my-first-project/src/components/country/country.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { CountryType } from "../../type";
 import "./country.css";
 
@@ -6,11 +7,30 @@ export interface CountryProps {
 }
 
 export default function Country({ country }: CountryProps) {
+  const [visited, setVisited] = useState<boolean>(false);
+
+  const handelVisited = () => {
+    // setVisited(true);
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    setVisited(!visited);
+  };
+
   return (
-    <div className="country">
+    <div className={`country  ${visited ? "country-visited" : "country"}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h2> Country Name : {country.name.common}</h2>
       <h2> Official Name : {country.name.official}</h2>
+      <p> ID : {country.ccn3.ccn3}</p>
+      <p>population : {country.population.population}</p>
+      <p>Capital : {country.capital.capital}</p>
+      <p>Area : {country.area.area}</p>
+      <button onClick={handelVisited} className="visited">
+        {visited ? "Visited" : "Mark as Visited"}
+      </button>
     </div>
   );
 }
--- Milestone5/module29/my-first-project/src/type.ts
+++ Milestone5/module29/my-first-project/src/type.ts
@@ -3,13 +3,36 @@ export interface CountryType {
     common: string;
     official: string;
   };
+
   ccn3: {
     ccn3: string;
   };
+
+  population: {
+    population: number;
+  };
+
   flags: {
     flags: {
       png: string;
       alt: string;
+    };
+  };
+  area: {
+    area: number;
+  };
+  capital: {
+    capital: string[];
+  };
+  continents: {
+    continents: string[];
+  };
+  currencies: {
+    currencies: {
+      JMD: {
+        name: string;
+        symbol: string;
+      };
     };
   };
 }
