30f0fc200524b94d896fda39aab5872f3a12a9ac
Piyas Ahmed <apiyas761@gmail.com>
2026-09-04 09:41:53 +0600

    commit

--- Milestone5/module28/api-post-get-app/src/post.tsx
+++ Milestone5/module28/api-post-get-app/src/post.tsx
@@ -1,8 +1,8 @@
 // import { use } from "react";
 // import PostCart from "./postCart";
 
-import { use } from "react";
-import PostCart from "./postCart";
+// import { use } from "react";
+// import PostCart from "./postCart";
 
 // export default function Posts({ userPostPromise }) {
 //   const posts = use(userPostPromise);
@@ -18,15 +18,15 @@ import PostCart from "./postCart";
 //   );
 // }
 
-export default function Post({ userPostPromise }) {
-  const posts = use(userPostPromise);
-  console.log(posts);
-  return (
-    <div>
-      <h3>User Post Data Loaded..</h3>
-      {posts.map((post) => (
-        <PostCart post={post}></PostCart>
-      ))}
-    </div>
-  );
-}
+// export default function Post({ userPostPromise }) {
+//   const posts = use(userPostPromise);
+//   console.log(posts);
+//   return (
+//     <div>
+//       <h3>User Post Data Loaded..</h3>
+//       {posts.map((post) => (
+//         <PostCart post={post}></PostCart>
+//       ))}
+//     </div>
+//   );
+// }
--- Milestone5/module28/api-post-get-app/src/postCart.tsx
+++ Milestone5/module28/api-post-get-app/src/postCart.tsx
@@ -7,14 +7,14 @@
 //   );
 // }
 
-import "./postCart.css";
+// import "./postCart.css";
 
-export default function PostCart({ post }) {
-  return (
-    <div className="posts">
-      <h4>Post ID : {post.id} </h4>
-      <strong>Title : {post.title} </strong>
-      <p>Body : {post.body} </p>
-    </div>
-  );
-}
+// export default function PostCart({ post }) {
+//   return (
+//     <div className="posts">
+//       <h4>Post ID : {post.id} </h4>
+//       <strong>Title : {post.title} </strong>
+//       <p>Body : {post.body} </p>
+//     </div>
+//   );
+// }
