bb738b8f1b400765783244b018a812585859fac7
Piyas Ahmed <apiyas761@gmail.com>
2026-09-08 17:34:03 +0600

    update

--- Milestone5/module31/BPL-Dream/src/components/Players/Players.tsx
+++ Milestone5/module31/BPL-Dream/src/components/Players/Players.tsx
@@ -46,7 +46,7 @@ const Players = ({ PlayersPromise }: PlayersPromiseProps) => {
       {/* </div> */}
 
       {/* Players Card */}
-      <div className="grid  sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+      <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-4 ">
         {buttonType === "Available" ? (
           <AvailablePlayers Players={Players} />
         ) : (
