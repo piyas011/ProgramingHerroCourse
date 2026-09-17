29d7ad54747aeda382256fb92732fa15e49f2179
Piyas Ahmed <apiyas761@gmail.com>
2026-09-17 17:20:56 +0600

    addCode

---
+++ Milestone6/nextjs-app/src/app/docs/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const docsPages = () => {
+  return (
+    <div className="flex justify-center items-center h-100">
+      <h1> docs Pages</h1>
+    </div>
+  );
+};
+
+export default docsPages;
