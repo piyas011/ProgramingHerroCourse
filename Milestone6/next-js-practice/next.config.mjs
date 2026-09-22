f948fff652499303fd474978a46678ef6ac7baa6
Piyas Ahmed <apiyas761@gmail.com>
2026-09-22 18:26:28 +0600

    Practice

--- Milestone6/next-js-practice/next.config.mjs
+++ Milestone6/next-js-practice/next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  //https://cdn.pixabay.com/photo/2015/06/19/20/13/sunset-815270_640.jpg
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- Milestone6/next-js-practice/src/app/contact/page.jsx
+++ Milestone6/next-js-practice/src/app/contact/page.jsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const ContactPage = () => {
   return (
     <div>
       <h1 className="text-5xl">Contact Page</h1>
+      <Image src="/images/img.jpg" alt="Images" width={500} height={500} />
+      <Image src="/images/piyas.jpg" alt="Images" width={500} height={500} />
+      <Image
+        src="https://cdn.pixabay.com/photo/2015/06/19/20/13/sunset-815270_640.jpg"
+        alt="Images"
+        width={500}
+        height={500}
+      />
+      <Image
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgG_47eIaubg55dDG180irshLZh9wAzlHLGRwApn_3ZQ&s=10"
+        alt="Images"
+        width={500}
+        height={500}
+      />
     </div>
   );
 };
